3ff0b8c90a164c46aedbefe9c356cbce3f8d6a10
Denis Mihaylov <denis.mihaylov@sap.com>
2020-09-05 22:40:13 +0300

    Simple Session Handling

--- app.js
+++ app.js
@@ -1,8 +1,30 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const uuid = require('uuid')
 const routes = require('./routes/index');
+const FileStore = require('session-file-store')(session);
 
 const app = express();
-app.use('/', routes);
-app.use('/static', express.static('static'))
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json())
+app.use(cookieParser());
+
+app.use(session({
+    genid: uuid.v4,
+    secret: 'keyboard cat',
+    resave: false,
+    store: new FileStore(),
+    saveUninitialized: true
+}));
+
+app.get('/', (req, res) => {
+    console.log('Inside the homepage callback function')
+    console.log(req.sessionID)
+    res.send(`You hit home page!\n`)
+});
+// app.use('/', routes);
+// app.use('/static', express.static('static'));
 
 module.exports = app;
